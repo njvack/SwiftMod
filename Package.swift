cf323d9e5897b577320b4eaaa0971771327c93e0
Nate Vack <nate@gimlet.us>
2026-02-19 19:29:52 -0600

    Remove MacSeq

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftMod",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v14)],
     products: [
         .library(name: "SwiftModCore", targets: ["SwiftModCore"]),
         .library(name: "SwiftModFormats", targets: ["SwiftModFormats"]),
